29e6e6ce3ecfd295f4e0bd551e70c8def5c0ba4d
Suraj <surajsppokale@gmail.com>
2026-06-16 13:00:43 +0530

    Floyd's for sequential numbers

--- Day_02_patterns.py
+++ Day_02_patterns.py
@@ -25,3 +25,13 @@ for i in range(1 , n):
     spaces = i
     stars = n - i 
     print(" " * +spaces +"* " *+stars)"""
+
+
+
+"""n = 4
+counter = 1
+for i in range(0,n+1):
+    for j in range(0,i):
+        print(counter, end = " ")
+        counter += 1
+    print()"""
