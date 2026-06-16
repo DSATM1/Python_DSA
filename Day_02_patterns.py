437d1baeb08fba6e5705768f2f60a8e609e46408
Suraj <surajsppokale@gmail.com>
2026-06-16 12:33:09 +0530

    diamond pattern generation example to patterns file

--- Day_02_patterns.py
+++ Day_02_patterns.py
@@ -13,3 +13,15 @@ for i in range(0,n):
     for j in range(0, i+1):
         print("*",end= " ")
     print()"""
+
+"""n = 5
+
+for i in range(1, n+1):
+    spaces = n - i
+    stars = i
+    print(" " * +spaces +"* " *+stars)
+
+for i in range(1 , n):
+    spaces = i
+    stars = n - i 
+    print(" " * +spaces +"* " *+stars)"""
