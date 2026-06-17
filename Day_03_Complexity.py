1903e8bcf094b169a0c1905f6cb1a00e6efdcff7
Suraj <surajsppokale@gmail.com>
2026-06-17 06:04:43 +0530

    Add constant time complexity example to Day_03_Complexity.py

--- Day_03_Complexity.py
+++ Day_03_Complexity.py
@@ -9,3 +9,9 @@ for i in range(n):
 m = 3
 for i in range(n+m):
     print(i, end = " ")"""
+
+
+"""# Constant Time O(1)
+# In this case it dosen't depend on input the input will be constant 
+for i in range(100):
+    print(i,end = " ")"""
