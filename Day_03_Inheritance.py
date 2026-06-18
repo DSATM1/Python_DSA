b3762eff21d51b141ecb0902bed465715fce7a63
Suraj <surajsppokale@gmail.com>
2026-06-18 19:05:50 +0530

    Admin class implementations

--- Day_03_Inheritance.py
+++ Day_03_Inheritance.py
@@ -1,2 +1,19 @@
 #-----Inhertance and Polymorphism-------
 
+"""class user:
+    def __init__(self,uname):
+        self.uname=uname
+        #self.__pwd = pwd
+
+    def login(self):
+        print(f"{self.uname} logged in ")
+
+    
+class Admin(user):
+    def delete_user(self):
+        print("Admin deleted user")
+
+a = Admin("Suraj")
+print(a.uname)
+a.delete_user()
+a.login()"""
