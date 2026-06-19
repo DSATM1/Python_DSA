c3fc159f73b2881ea5ffb7b7bd8119be7dfbf305
Suraj <surajsppokale@gmail.com>
2026-06-19 10:54:50 +0530

    Add Day_04.py with implementations for dictionary creation and digital root calculation

---
+++ Day_04.py
@@ -0,0 +1,21 @@
+"""key = ['a','b','c']
+value = [1,2,3]
+mu_dict = {k:v for k,v in zip(key,value)} 
+print(mu_dict)"""
+
+
+
+"""def sum_of_digits(n):
+    total = 0
+    while n > 0:
+        total += n % 10
+        n = n //10
+    return total
+
+n = int(input("Enter N:").strip())
+step = 1
+while n>=10:
+    n = sum_of_digits(n)
+    step += 1
+
+print(f"Digital Root : {n}")"""
