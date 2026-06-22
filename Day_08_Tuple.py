4734034f1832416f816c88b83672907c0f616b7c
Suraj <surajsppokale@gmail.com>
2026-06-22 07:23:44 +0530

    Add initial implementation of tuple examples and operations

---
+++ Day_08_Tuple.py
@@ -0,0 +1,11 @@
+"""coordinates = (10, 20,30) # 2D point 
+rgb = (255, 0, 128) # RGB color 
+mixed = (1, "hello", 3.14, True) # mixed types 
+single = (42,) # single item needs comma! 
+empty = () # empty tuple 
+print(len(coordinates)) # 2 
+print(type(coordinates)) # <class 'tuple'>
+print(rgb)
+print(mixed)
+print(single)
+print(empty)"""
