fc0087a915c15d09cef53b34ff45dc58c6bd8a4b
Suraj <surajsppokale@gmail.com>
2026-06-22 06:56:37 +0530

    Add bubble sort implementation with example usage

--- Day_07_Algo.py
+++ Day_07_Algo.py
@@ -1,1 +1,21 @@
 #---Searching And Sorting -----
+# Bubble Sort
+
+
+"""def sortArray(nums):
+    n = len(nums)
+
+    for i in range(n):
+        isSwap = False
+        for j in range(n-i-1):
+            if nums[j]>nums[j+1]:
+                temp = nums[j]
+                nums[j] = nums[j+1]
+                nums[j+1] = temp
+                isSwap = True
+        if not isSwap:
+            break
+    
+    return nums    
+    
+print(sortArray([32,43,1,56,98,9,32312,787,112,9866]))"""
