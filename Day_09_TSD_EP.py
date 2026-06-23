37bdfec5cc766059c5b80cf369c7e560f6c7edb8
Suraj <surajsppokale@gmail.com>
2026-06-23 07:46:40 +0530

    Add sorting functionality for student departments in Day_09_TSD_EP.py

--- Day_09_TSD_EP.py
+++ Day_09_TSD_EP.py
@@ -17,3 +17,29 @@
 freq,dup = count_num([1, 2, 2, 3, 3, 3, 4, 4, 4, 4])
 print(f"Frequency  : {freq}")
 print(f"Duplicates : {dup}")"""
+
+
+
+# students = [("Suraj","MCA"),("Putta","BCA"),("Ravi","MCA"),("Kiran","BCA"),("Meena","MBA")]
+
+# MCA : ["Suraj", "Ravi"]
+# BCA : ["Putta", "Kiran"]
+# MBA : ["Meena"]
+
+"""def sort_dept(students):
+        group = {}
+        for name, dept in students:
+            if dept not in group:
+                group[dept] = []
+            group[dept].append(name)
+        return group
+result = sort_dept([("Suraj","MCA"),("Putta","BCA"),
+            ("Ravi","MCA"),("Kiran","BCA"),
+            ("Meena","MBA")])
+
+for dept,name in result.items():
+     print(f"{dept} : {name}")
+     
+# print(f"MCA : {result["MCA"]}")
+# print(f"BCA : {result["BCA"]}")
+# print(f"MBA : {result["MBA"]}")"""
