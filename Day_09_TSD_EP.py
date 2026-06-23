389ae262ffdf8512564ac26c14a4b4599785fd1e
Suraj <surajsppokale@gmail.com>
2026-06-23 07:08:42 +0530

    Add extra practice for counting frequency and duplicates in a list

---
+++ Day_09_TSD_EP.py
@@ -0,0 +1,19 @@
+#---Extra Practice----
+
+# nums = [1, 2, 2, 3, 3, 3, 4, 4, 4, 4]
+# Frequency : {1:1, 2:2, 3:3, 4:4}
+# Duplicates: {2:2, 3:3, 4:4}
+
+"""def count_num(lst):
+    count = {}
+    for l in lst:
+        count[l] = count.get(l,0)+1
+    dct = {}
+    for key,value in count.items():
+        if value > 1:
+            dct[key] = value 
+    
+    return count, dct
+freq,dup = count_num([1, 2, 2, 3, 3, 3, 4, 4, 4, 4])
+print(f"Frequency  : {freq}")
+print(f"Duplicates : {dup}")"""
