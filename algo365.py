a0b202c21ad0c696004b80e7caf6d6389f87445a
Suraj <surajsppokale@gmail.com>
2026-06-25 13:32:22 +0530

    Add initial implementation of number comparison algorithm with detailed steps

---
+++ algo365.py
@@ -0,0 +1,17 @@
+# 5 13 15 7 6
+
+# CPU thinking level 
+
+# Step 1 = we need to read the all numbers one by one 
+# Step 2 = assume the first number is largest so largest = 5 
+# Step 3 = read the bext bumber   largest = 13
+# Step 4 = compare new value which is (13) to the cureent value which is (5) if the new value is > current value then update and remember the largest value is 13(new value)
+# Step 5 = read the next number (15) and again compare with current largest value (13) if it's > then copy to largest if that is success then copy the largest to second largest.
+# Step 6 = read next number (7) if it's smaller no action to be taken if it's greater than largest then update 
+# Step 7 = read next number (6) same as above if it's greater take the action else no action to be taken 
+
+
+# 19 5 13 15 7 6
+
+# first valu is greater 
+
