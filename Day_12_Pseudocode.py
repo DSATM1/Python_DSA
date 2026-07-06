0f220b0faa617d38d22e5242c2c6afc68e4e88eb
Suraj <surajsppokale@gmail.com>
2026-07-06 18:59:05 +0530

    Comment out example list in pseudocode section

--- Day_12_Pseudocode.py
+++ Day_12_Pseudocode.py
@@ -8,7 +8,7 @@
 
 
 ####------Code-----#####
-lst = [3,9,7,5,14,0]
+"""lst = [3,9,7,5,14,0]
 
 largest = lst[0]
 
@@ -16,5 +16,5 @@ for item in lst:
     if item > largest:
         largest = item
 print(largest)  
-
+"""
 
