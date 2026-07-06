4eaeb4d2cb43270d3bf8bce1358a7cbcecad8c41
Suraj S P <161311066+DSATM1@users.noreply.github.com>
2026-07-06 17:34:57 +0530

    Fix print statement formatting in pseudocode

--- Day_12_Pseudocode.py
+++ Day_12_Pseudocode.py
@@ -1,3 +1,13 @@
+
+###----- Pseudocode----#####
+largest = first element of list
+for each number in list:
+    if number > largest:
+        largest = number       ← update largest, not number
+return largest
+
+
+####------Code-----#####
 lst = [3,9,7,5,14,0]
 
 largest = lst[0]
@@ -6,3 +16,5 @@ for item in lst:
     if item > largest:
         largest = item
 print(largest)  
+
+
